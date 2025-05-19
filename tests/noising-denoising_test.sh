defe92951c6b0b8d52259de05d1f156c57058675
Yanheng Li <48171400+egg5154@users.noreply.github.com>
2025-05-19 17:01:14 +0800

    Update noising-denoising_test.sh

--- tests/noising-denoising_test.sh
+++ tests/noising-denoising_test.sh
@@ -2,10 +2,10 @@
 
 export SCRIPT_DIR=$(dirname $(readlink -f $0))
 python -u $SCRIPT_DIR/noising-denoising_test.py \
-    --config_path $SCRIPT_DIR/../checkpoints/diffusion-transformer/config.pkl \
-    --params_path $SCRIPT_DIR/../checkpoints/diffusion-transformer/dit_params_step1000000.pkl \
+    --config_path $SCRIPT_DIR/../checkpoints/diffusion-transformer/config_opt.pkl \
+    --params_path $SCRIPT_DIR/../checkpoints/diffusion-transformer/dit_params_opt.pkl \
     --vae_config_path $SCRIPT_DIR/../checkpoints/auto-encoder/config.pkl \
-    --vae_params_path $SCRIPT_DIR/../checkpoints/auto-encoder/ae_params_step265000.pkl \
+    --vae_params_path $SCRIPT_DIR/../checkpoints/auto-encoder/ae_params_opt.pkl \
     --alphabet_path $SCRIPT_DIR/../train/smiles_alphabet.pkl \
     --random_seed 8888 \
     --sampling_method beam \
